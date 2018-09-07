5f554c6c575387fd530f5026e604b524050a1226
juldebar <juldebar@gmail.com>
2018-09-07 12:00:08 +0200

    fix issues in Postgres workflow: creation of one view per dataset => view name issue

--- metadata_workflow_Postgres_Postgis/scripts/dynamic_metadata_queries.R
+++ metadata_workflow_Postgres_Postgis/scripts/dynamic_metadata_queries.R
@@ -20,15 +20,15 @@ getSQLQueries <- function(config, metadata){
   logger.info("######################################################################################################")
   logger.info("Setting SQL queries")
   logger.info("######################################################################################################")
-  # metadata$view_name <-metadata$Identifier
-  SQL$query_dynamic_metadata_spatial_Extent <- paste("SELECT ST_AsText(ST_Envelope(ST_ConvexHull(ST_Collect(geom)))) As geom FROM",metadata$view_name,";",sep=" ")
-  SQL$query_dynamic_metadata_count_features <-  paste("SELECT count(*) FROM",metadata$view_name,";",sep=" ")
-  SQL$query_dynamic_metadata_get_geometry_name <-  paste("SELECT type FROM geometry_columns WHERE f_table_name='",metadata$view_name,"';",sep="")
-  SQL$query_dynamic_metadata_get_geometry_type <-  paste("SELECT type FROM geometry_columns WHERE f_table_name='",metadata$view_name,"';",sep="")
-  SQL$query_dynamic_metadata_get_geometry_SRID <-  paste("SELECT SRID FROM geometry_columns WHERE f_table_name='",metadata$view_name,"';",sep="")
-  SQL$query_dynamic_metadata_temporal_Extent <- paste("SELECT 'start='::text || MIN(date)::text || ';end='::text ||MAX(date)::text AS temporal_extent FROM",metadata$view_name,";",sep=" ")
-  # SQL$query_dynamic_list_keywords <- paste("SELECT DISTINCT (tag) FROM",metadata$view_name,";",sep=" ")
-  SQL$query_CSV <- paste("SELECT * FROM  ",metadata$view_name," ;",sep=" ")# julien To BE Done => transform WKB in WKT ?
+  metadata$related_view_name
+  SQL$query_dynamic_metadata_spatial_Extent <- paste("SELECT ST_AsText(ST_Envelope(ST_ConvexHull(ST_Collect(geom)))) As geom FROM",metadata$related_view_name,";",sep=" ")
+  SQL$query_dynamic_metadata_count_features <-  paste("SELECT count(*) FROM",metadata$related_view_name,";",sep=" ")
+  SQL$query_dynamic_metadata_get_geometry_name <-  paste("SELECT type FROM geometry_columns WHERE f_table_name='",metadata$related_view_name,"';",sep="")
+  SQL$query_dynamic_metadata_get_geometry_type <-  paste("SELECT type FROM geometry_columns WHERE f_table_name='",metadata$related_view_name,"';",sep="")
+  SQL$query_dynamic_metadata_get_geometry_SRID <-  paste("SELECT SRID FROM geometry_columns WHERE f_table_name='",metadata$related_view_name,"';",sep="")
+  SQL$query_dynamic_metadata_temporal_Extent <- paste("SELECT 'start='::text || MIN(date)::text || ';end='::text ||MAX(date)::text AS temporal_extent FROM",metadata$related_view_name,";",sep=" ")
+  # SQL$query_dynamic_list_keywords <- paste("SELECT DISTINCT (tag) FROM",metadata$related_view_name,";",sep=" ")
+  SQL$query_CSV <- paste("SELECT * FROM  ",metadata$related_view_name," ;",sep=" ")# julien To BE Done => transform WKB in WKT ?
   SQL$query_wfs_wms <- SQL$query_CSV # julien To BE Done => change query if CSV = transform WKB in WKT
   logger.info("######################################################################################################")
   logger.info("Execute all SQL queries")
