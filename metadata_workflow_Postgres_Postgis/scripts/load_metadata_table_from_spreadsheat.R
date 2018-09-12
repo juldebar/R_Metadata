952d6cb4825ab92211e243add94c232743f0dd8a
root <root@ordibibi2.ifremer.fr>
2018-09-12 08:28:01 +0200

    set metadata publication actions by using CSW-T by default

--- metadata_workflow_Postgres_Postgis/scripts/load_metadata_table_from_spreadsheat.R
+++ metadata_workflow_Postgres_Postgis/scripts/load_metadata_table_from_spreadsheat.R
@@ -1,7 +1,7 @@
 #metadata_dataframe prepares a dataframe from a google spreadsheet to load the metadata table of the postgres database
 #@param Dublin_Core_metadata
 #@returns an object of class dataframe
-metadata_dataframe <- function(Dublin_Core_metadata){
+metadata_dataframe <- function(config, Dublin_Core_metadata){
   
   logger.info <- config$logger.info
   logger.info("---------------------------------------------------------------------------------")  
@@ -39,7 +39,7 @@ metadata_dataframe <- function(Dublin_Core_metadata){
     
     all_metadata <- bind_rows(all_metadata, metadata)
   }
-    return(all_metadata)
+  return(all_metadata)
 }
 
 
@@ -55,9 +55,10 @@ create_one_view_per_dataset <- function(config, metadata){
   number_row<-nrow(metadata)
   for (i in 1:number_row ) {
     view_name <- metadata$related_view_name[i]
-    sql_view <- metadata$related_sql_query[i]
+    sql_view <- gsub(";","",metadata$related_sql_query[i])
     # sql_view <- paste('SELECT ogc_fid, wkb_geometry AS geom, filename, gpslatitud AS lat,gpslongitu AS lon, gpsdatetim AS date,lightvalue,imagesize,model,path,parent_dir FROM "public"."photos_metadata" WHERE parent_dir = \'',view_name,'\';')
-    SQLquery <- paste('DROP MATERIALIZED VIEW IF EXISTS ',view_name,' CASCADE ; CREATE MATERIALIZED VIEW  IF NOT EXISTS ',view_name,' AS ', sql_view,' WITH DATA ', sep="");
+    SQLquery <- paste('DROP MATERIALIZED VIEW IF EXISTS "',view_name,'" ; CREATE MATERIALIZED VIEW "',view_name,'" AS ', sql_view,' WITH DATA ', sep="");
+    # @julien TO BE DONE => ADD SPATIAL INDEX
     resuling_view <- dbGetQuery(con, SQLquery)
   }
   logger.info("MATERIALIZED VIEW has been created")  
